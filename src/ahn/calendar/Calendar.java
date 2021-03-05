b509b72c282dfbd461d05ab01b23cb504ee6bf40
Ahn <ahn01045503326@gmail.com>
2021-03-05 22:06:04 +0800

    Update month of maxdays

---
+++ src/ahn/calendar/Calendar.java
@@ -0,0 +1,32 @@
+package ahn.calendar;
+
+import java.util.Scanner;
+
+public class Calendar {
+	//	상수
+	private final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+	//	함수
+	public int maxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+
+	public void printSampleCalendar() {
+		System.out.println("일 월 화 수 목 금 토 일 ");
+		System.out.println("----------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
+	
+	public static void main(String[] args) {	
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		System.out.println("달을 입력하세요.");		
+		int month = scanner.nextInt();
+		System.out.printf("%d월은 %d일까지 입니다.",month,cal.maxDaysOfMonth(month));
+		cal.printSampleCalendar();
+		scanner.close();
+	}
+}
