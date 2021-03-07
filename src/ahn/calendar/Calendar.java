cfe0f8295a36ece05251a20f50585541cdca80a8
Ahn <ahn01045503326@gmail.com>
2021-03-07 21:35:41 +0800

    Update repeat function

--- src/ahn/calendar/Calendar.java
+++ src/ahn/calendar/Calendar.java
@@ -20,13 +20,30 @@ public class Calendar {
 		System.out.println("22 23 24 25 26 27 28");
 	}
 	
+	public void months_while(int times) {
+	}
+	
 	public static void main(String[] args) {	
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요.");		
-		int month = scanner.nextInt();
-		System.out.printf("%d월은 %d일까지 입니다.",month,cal.maxDaysOfMonth(month));
-		cal.printSampleCalendar();
+		System.out.println("반복횟수를 입력하세요.");		
+		int times = scanner.nextInt();
+		System.out.println("월를 입력하세요.");
+		
+		int [] arr_months = new int [times];
+		int i = 0;
+		while(i<times) {
+			arr_months[i] = scanner.nextInt();
+			i++;
+		}
+		
+		for(int j=0;j<times;j++) {
+			System.out.printf("%d월은 %d일까지 있습니다.\n",arr_months[j],cal.maxDaysOfMonth(arr_months[j]));
+		}
+		
+		
+		System.out.println("Bye~");
+//		cal.printSampleCalendar();
 		scanner.close();
 	}
 }
